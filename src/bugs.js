9f7e0feeaa70552eb4210b997e4338c49ac8af58
ketherball <121590283+ketherball@users.noreply.github.com>
2023-07-03 14:22:17 -0400

    feat: add api to bugs
    API contains methods to act on bugsList, which remains private and inaccesible directly.

--- src/bugs.js
+++ src/bugs.js
@@ -4,6 +4,34 @@ let bugsList = [
     { id: uniqid(), title: "Bug 1" },
     { id: uniqid(), title: "Bug 2" },
     { id: uniqid(), title: "Bug 3" }
-]
+];
 
-export default bugsList;
+function getBugs() {
+    return [...bugsList];
+}
+
+function add(bug) {
+    bugsList.push(bug);
+};
+
+function edit(id, change) {
+    return bugsList = bugsList.map(bug => {
+        if (bug.id === id) {
+            return change;
+        } else return bug;
+    });
+};
+
+function remove(id) {
+    return bugsList = bugsList.filter(bug =>
+        bug.id !== id);
+};
+
+const bugs = {
+    getBugs: getBugs,
+    add: add,
+    edit: edit,
+    delete: remove
+}
+
+export default bugs;
