5924cf9c0ef62f06ba77c59f58ede294cae5d971
ketherball <121590283+ketherball@users.noreply.github.com>
2023-07-03 13:27:52 -0400

    refactor: change bugs data structure to array

--- src/bugs.js
+++ src/bugs.js
@@ -1,23 +1,9 @@
 import uniqid from 'uniqid';
 
-let bugsList = {}
-
-bugsList[uniqid()] = {
-    title: 'Bug 1',
-    description: 'Lorum Ipsum.',
-    status: 'Not Started',
-}
-
-bugsList[uniqid()] = {
-    title: 'Bug 2',
-    description: 'Lorum Ipsum.',
-    status: 'Not Started',
-}
-
-bugsList[uniqid()] = {
-    title: 'Bug 3',
-    description: 'Lorum Ipsum.',
-    status: 'Not Started',
-}
+let bugsList = [
+    { id: uniqid(), title: "Bug 1" },
+    { id: uniqid(), title: "Bug 2" },
+    { id: uniqid(), title: "Bug 3" }
+]
 
 export default bugsList;
