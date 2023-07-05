04c002dbd732ec69a2f9cc29bf78006d7f5a765a
ketherball <121590283+ketherball@users.noreply.github.com>
2023-07-05 18:34:11 -0400

    feat: add bug tracker card to dashboard
    Dashboard now has draggable, resizable 'cards' system. It currently includes the 'Bugs' card which tracks the amount of bugs in storage.

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,10 +1,16 @@
 import "./Dashboard.css";
+import { useState } from "react";
+import ResizableGrid from "./ResizableGrid/ResizableGrid";
+import bugs from "../../bugs";
 
 function Dashboard() {
+
+    const [bugsLength, setBugsLength] = useState(bugs.getBugs().length);
 
     return(
         <div>
             <h2 className="page__header">Dashboard</h2>
+            <ResizableGrid bugsLength={bugsLength}/>
         </div>
     );
 }
---
+++ src/pages/Dashboard/ResizableGrid/ResizableGrid.js
@@ -0,0 +1,90 @@
+import "/node_modules/react-grid-layout/css/styles.css";
+import "/node_modules/react-resizable/css/styles.css";
+import "./ResizableGrid.css";
+import { Responsive, WidthProvider } from "react-grid-layout";
+
+const ResponsiveGridLayout = WidthProvider(Responsive);
+
+function ResizableGrid(props) {
+
+    const { bugsLength } = props;
+
+    const layout1 = [
+        { i: "a", x: 0, y: 0, w: 10, h: 15, static: false },
+        { i: "b", x: 10, y: 0, w: 10, h: 15 },
+        { i: "c", x: 0, y: 10, w: 10, h: 15 }
+    ]
+
+    const layout2 = [
+        { i: "a", x: 0, y: 0, w: 15, h: 15, static: false },
+        { i: "b", x: 15, y: 0, w: 15, h: 15 },
+        { i: "c", x: 0, y: 15, w: 15, h: 15 }
+    ]
+
+    const layout3 = [
+        { i: "a", x: 0, y: 0, w: 20, h: 15, static: false },
+        { i: "b", x: 20, y: 0, w: 20, h: 15 },
+        { i: "c", x: 40, y: 0, w: 20, h: 15 }
+    ]
+
+    const layout4 = [
+        { i: "a", x: 0, y: 0, w: 33, h: 15, static: false },
+        { i: "b", x: 33, y: 0, w: 33, h: 15 },
+        { i: "c", x: 66, y: 0, w: 33, h: 15 }
+    ]
+
+    const layout5 = [
+        { i: "a", x: 0, y: 0, w: 40, h: 15, static: false },
+        { i: "b", x: 40, y: 0, w: 40, h: 15 },
+        { i: "c", x: 80, y: 0, w: 40, h: 15 }
+    ]
+
+    const layouts = { lg: layout5, md: layout4, sm: layout3, xs: layout2, xxs: layout1 };
+  
+    return (
+        <ResponsiveGridLayout
+            className="layout resizableGrid"
+            layouts={layouts}
+            breakpoints={{ lg: 1200, md: 996, sm: 768, xs: 480, xxs: 0 }}
+            cols={{ lg: 160, md: 99, sm: 60, xs: 30, xxs: 10 }}
+            rowHeight={1}
+            compactType={"vertical"}
+            isBounded={true}
+            resizeHandles={['s', 'e', 'se']}
+            margin={[20, 20]}
+        >
+            <div key="a" className="grid-comp">
+                <Card title={"Bugs"} bugsLength={bugsLength}/>
+            </div>
+            <div key="b" className="grid-comp">
+                <Card title={"Test Card"}/>
+            </div>
+            <div key="c" className="grid-comp">
+                <Card title={"Test Card"}/>
+            </div>
+        </ResponsiveGridLayout>
+    );
+}
+
+export default ResizableGrid;
+
+function Card(props) {
+
+    const { title, bugsLength } = props;
+
+    return(
+        <>
+            <div style={{display: "flex", justifyContent: "space-between", alignItems: "center"}}>
+                <h3 style={{color: "rgb(182, 182, 182)", fontWeight: "500", margin: ".7em"}}>{title}</h3>
+                <div style={{display: "flex", margin: ".1em .6em", padding: ".6em .1em", cursor: "pointer"}}>
+                    <div style={{backgroundColor: "#696969", margin: ".1em", height: "2px", width: "2px", borderRadius: "5px"}}></div>
+                    <div style={{backgroundColor: "#696969", margin: ".1em", height: "2px", width: "2px", borderRadius: "5px"}}></div>
+                    <div style={{backgroundColor: "#696969", margin: ".1em", height: "2px", width: "2px", borderRadius: "5px"}}></div>
+                </div>
+            </div>
+            <div style={{display: "flex", justifyContent: "center", alignItems: "center", marginTop: "1.5rem"}}>
+                <p style={{color: "rgb(182, 182, 182)", fontSize: "9rem", margin: "0"}}>{bugsLength}</p>
+            </div>
+        </>
+    );
+}
