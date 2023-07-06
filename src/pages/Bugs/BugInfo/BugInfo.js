0051836975a4d697215402b0584c3673af98b1b0
ketherball <121590283+ketherball@users.noreply.github.com>
2023-07-06 10:23:43 -0400

    refactor: abstract edit form into its own component

--- src/pages/Bugs/BugInfo/BugInfo.js
+++ src/pages/Bugs/BugInfo/BugInfo.js
@@ -4,57 +4,7 @@ import "./BugInfo.css";
 function BugInfo(props) {
 
     const { bugs, bug, setBug, setBugsList } = props;
-
     const [editable, setEditable] = useState(false);
-    const [name, setName] = useState("");
-    const [dueDate, setDueDate] = useState("");
-    const [description, setDescription] = useState("");
-    const [status, setStatus] = useState(0);
-
-    const handleCancel = e => {
-        e.preventDefault();
-        setEditable(false);
-    };
-
-    const handleNameChange = e => {
-        setName(e.target.value);
-    };
-
-    const handleDescriptionChange = e => {
-        setDescription(e.target.value);
-    };
-
-    const handleDueDateChange = e => {
-        setDueDate(e.target.value);
-    };
-
-    const handleStatusChange = e => {
-        setStatus(Number(e.target.value));
-    };
-
-    const handleSubmit = e => {
-        e.preventDefault();
-        bugs.edit(bug.id, { // temp code
-            id: bug.id, 
-            name: name !== "" ? name : bug.name, 
-            dueDate: dueDate !== "" ? new Date(dueDate) : bug.dueDate, 
-            description: description !== "" ? description : bug.description, 
-            status: status !== "" ? status : bug.status 
-        });
-        setBugsList(bugs.getBugs());
-        setBug({ // temp code: find solution where data reflects src (bugsList)
-            id: bug.id, 
-            name: name !== "" ? name : bug.name, 
-            dueDate: dueDate !== "" ? new Date(dueDate) : bug.dueDate, 
-            description: description !== "" ? description : bug.description, 
-            status: status !== "" ? status : bug.status 
-        });
-        setName("");
-        setDueDate("");
-        setDescription("");
-        setStatus(0);
-        setEditable(false);
-    };
 
     const handleDeletion = () => {
         bugs.delete(bug.id);
@@ -66,31 +16,7 @@ function BugInfo(props) {
         <div className="bugInfo">
             {editable ? 
                 <>
-                    <form style={{display: "flex", flexDirection: "column", alignItems: "start"}}>
-                        <label>Name</label>
-                        <input type="text" onChange={handleNameChange} placeholder={bug.name}/>
-                        <label>Due Date</label>
-                        <input type="date" onChange={handleDueDateChange}/>
-                        <label>Description</label>
-                        <textarea onChange={handleDescriptionChange}/>
-                        <label>Status:</label>
-                        <div style={{display: "flex", justifyContent: "space-between", width: "110px"}}>
-                            <label>Not Started</label>
-                            <input type="radio" name="status" value={1} onChange={handleStatusChange}/>
-                        </div>
-                        <div style={{display: "flex", justifyContent: "space-between", width: "110px"}}>
-                            <label>In Progress</label>
-                            <input type="radio" name="status" value={2} onChange={handleStatusChange}/>
-                        </div>
-                        <div style={{display: "flex", justifyContent: "space-between", width: "110px"}}>
-                            <label>Resolved</label>
-                            <input type="radio" name="status" value={3} onChange={handleStatusChange}/>
-                        </div>
-                        <div>
-                            <button onClick={handleSubmit}>Confirm</button>
-                            <button onClick={handleCancel}>Cancel</button>
-                        </div>
-                    </form>
+                    <Form bugs={bugs} bug={bug} setBug={setBug} setBugsList={setBugsList} setEditable={setEditable}/>
                     <button onClick={handleDeletion}>Delete</button>
                 </>
             :
@@ -137,4 +63,87 @@ function convertStatus(status) {
             break;
     }
     return statusStr;
+}
+
+function Form(props) {
+
+    const [name, setName] = useState("");
+    const [dueDate, setDueDate] = useState("");
+    const [description, setDescription] = useState("");
+    const [status, setStatus] = useState(0);
+
+    const { bugs, bug, setBug, setBugsList, setEditable } = props;
+
+    const handleCancel = e => {
+        e.preventDefault();
+        setEditable(false);
+    };
+
+    const handleNameChange = e => {
+        setName(e.target.value);
+    };
+
+    const handleDescriptionChange = e => {
+        setDescription(e.target.value);
+    };
+
+    const handleDueDateChange = e => {
+        setDueDate(e.target.value);
+    };
+
+    const handleStatusChange = e => {
+        setStatus(Number(e.target.value));
+    };
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        bugs.edit(bug.id, { // temp code
+            id: bug.id, 
+            name: name !== "" ? name : bug.name, 
+            dueDate: dueDate !== "" ? new Date(dueDate) : bug.dueDate, 
+            description: description !== "" ? description : bug.description, 
+            status: status !== 0 ? status : bug.status 
+        });
+        setBugsList(bugs.getBugs());
+        setBug({ // temp code: find solution where data reflects src (bugsList)
+            id: bug.id, 
+            name: name !== "" ? name : bug.name, 
+            dueDate: dueDate !== "" ? new Date(dueDate) : bug.dueDate, 
+            description: description !== "" ? description : bug.description, 
+            status: status !== 0 ? status : bug.status 
+        });
+        setName("");
+        setDueDate("");
+        setDescription("");
+        setStatus(0);
+        setEditable(false);
+    };
+
+    return(
+        <form style={{display: "flex", flexDirection: "column", alignItems: "start"}}>
+            <label>Name</label>
+            <input type="text" onChange={handleNameChange} placeholder={bug.name}/>
+            <label>Due Date</label>
+            <input type="date" onChange={handleDueDateChange}/>
+            <label>Description</label>
+            <textarea onChange={handleDescriptionChange}/>
+            <label>Status:</label>
+            <div style={{display: "flex", justifyContent: "space-between", width: "110px"}}>
+                <label>Not Started</label>
+                <input type="radio" name="status" value={1} onChange={handleStatusChange}/>
+            </div>
+            <div style={{display: "flex", justifyContent: "space-between", width: "110px"}}>
+                <label>In Progress</label>
+                <input type="radio" name="status" value={2} onChange={handleStatusChange}/>
+            </div>
+            <div style={{display: "flex", justifyContent: "space-between", width: "110px"}}>
+                <label>Resolved</label>
+                <input type="radio" name="status" value={3} onChange={handleStatusChange}/>
+            </div>
+            <div>
+                <button onClick={handleSubmit}>Confirm</button>
+                <button onClick={handleCancel}>Cancel</button>
+            </div>
+        </form>
+    )
 }
